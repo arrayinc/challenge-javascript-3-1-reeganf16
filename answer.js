e96f50707b194dba3bd711bbddb255ce494887ca
reeganford <rmford1695@gmail.com>
2019-12-05 11:53:40 -0700

    Finished chellenge #1

--- answer.js
+++ answer.js
@@ -1,1 +1,10 @@
 // *** YOUR ANSWER BELOW ***
+
+function beerSong(startN) {
+    for(let i = startN; i > 0; i--) {
+        console.log(`${i} bottles of beer on the wall. ${i} bottles of beer. You take one down. Pass it around. ${i-1} bottles of beer on the wall.`);
+    }
+    console.log('The beer is gone!');
+}
+
+beerSong(99);
